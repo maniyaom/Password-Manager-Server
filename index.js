159d43a6b9d41e1d65682b10bfe6e03054ee31be
Om Maniya <ommaniya01@gmail.com>
2024-06-25 14:00:33 +0530

    Fixed bugs

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const CryptoJS = require("crypto-js");
 require('dotenv').config();
 const cors = require('cors');
 const app = express();
@@ -7,6 +8,43 @@ const admin = require("firebase-admin");
 const { getFirestore } = require('firebase-admin/firestore');
 
 const credentials = JSON.parse(process.env.FIREBASE_CONFIGURATION);
+
+
+function generateKey(service) {
+    let temp = "";
+    let key = "";
+
+    for (let i = 0; i < service.length; i++) {
+        if (service[i].charCodeAt(0) >= 97)
+            temp += service[i].charCodeAt(0).toString() + (service[i].charCodeAt(0) - 1).toString();
+        else
+            temp += service[i].charCodeAt(0).toString() + (service[i].charCodeAt(0) + 1).toString();
+
+        key += temp[i] + service[i];
+    }
+
+    key += temp.slice(service.length);
+    return key;
+}
+
+function encrypt(service, plaintext) {
+
+    //Generate Key for encryption
+    const key = generateKey(service);
+
+    // Encrypt Password
+    return CryptoJS.AES.encrypt(plaintext, key).toString();
+}
+
+function decrypt(service, ciphertext) {
+
+    //Generate Key for encryption
+    const key = generateKey(service);
+
+    // Encrypt Password
+    const bytes = CryptoJS.AES.decrypt(ciphertext, key);
+    return bytes.toString(CryptoJS.enc.Utf8);
+}
 
 admin.initializeApp({
     credential: admin.credential.cert(credentials)
@@ -20,21 +58,31 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
 app.post('/addPassword', async (req, res) => {
+    if (!(req.body.uid && req.body.service && req.body.username && req.body.password && req.headers.authorization))
+        return res.status(400).json({ message: 'Request should contain uid, service, username, password and authorization header.' });
+    req.body.password = encrypt(req.body.service, req.body.password);
+    req.body.username = encrypt(req.body.service, req.body.username);
     const passwords = db.collection('passwords');
     const authToken = req.headers.authorization.split(' ')[1];
     try {
         const decodeValue = await admin.auth().verifyIdToken(authToken);
         if (decodeValue && decodeValue.uid == req.body.uid && decodeValue.email_verified == true) {
             await passwords.add(req.body);
-            return res.status(200).json({ message : "Password Added Successfully"});
+            return res.status(200).json({ message: "Password Added Successfully" });
         }
-        return res.status(400).json({ message: 'Unauthorized Access' });
+        return res.status(401).json({ message: 'Unauthorized Access' });
     } catch (e) {
         return res.status(500).json({ message: 'Internal Server Error' });
     }
 });
 
 app.post('/editPassword', async (req, res) => {
+    if (!(req.body.id && req.body.uid && req.body.service && req.body.username && req.body.password && req.headers.authorization))
+        return res.status(400).json({ message: 'Request should contain password id, user id, service, username, password and authorization header.' });
+
+    req.body.password = encrypt(req.body.service, req.body.password);
+    req.body.username = encrypt(req.body.service, req.body.username);
+
     const id = req.body.id;
     delete req.body.id;
     const passwords = db.collection('passwords');
@@ -43,7 +91,7 @@ app.post('/editPassword', async (req, res) => {
         const decodeValue = await admin.auth().verifyIdToken(authToken);
         if (decodeValue && decodeValue.uid == req.body.uid && decodeValue.email_verified == true) {
             await passwords.doc(id).update(req.body);
-            return res.status(200).json({ message : "Password Updated Successfully"});
+            return res.status(200).json({ message: "Password Updated Successfully" });
         }
         return res.status(400).json({ message: 'Unauthorized Access' });
     } catch (e) {
@@ -52,6 +100,8 @@ app.post('/editPassword', async (req, res) => {
 });
 
 app.post('/deletePassword', async (req, res) => {
+    if (!(req.body.id && req.body.uid && req.headers.authorization))
+        return res.status(400).json({ message: 'Request should contain password id, user id and authorization header.' });
     const id = req.body.id;
     const passwords = db.collection('passwords');
     const authToken = req.headers.authorization.split(' ')[1];
@@ -59,7 +109,7 @@ app.post('/deletePassword', async (req, res) => {
         const decodeValue = await admin.auth().verifyIdToken(authToken);
         if (decodeValue && decodeValue.uid == req.body.uid && decodeValue.email_verified == true) {
             await passwords.doc(id).delete();
-            return res.status(200).json({ message : "Password Deleted Successfully"});
+            return res.status(200).json({ message: "Password Deleted Successfully" });
         }
         return res.status(400).json({ message: 'Unauthorized Access' });
     } catch (e) {
@@ -67,31 +117,35 @@ app.post('/deletePassword', async (req, res) => {
     }
 });
 
-app.post('/getPassword', async (req, res) => {
-    const passwords = db.collection('passwords');
+app.post('/decryptPasswords', async (req, res) => {
+    if (!(req.body.passwords && req.headers.authorization))
+        return res.status(400).json({ message: 'Request should contain passwords and authorization header.' });
     const authToken = req.headers.authorization.split(' ')[1];
-
-    try{
+    let passkeys = req.body.passwords;
+    let valid = true;
+    try {
         const decodeValue = await admin.auth().verifyIdToken(authToken);
-        if (decodeValue && decodeValue.uid == req.body.uid && decodeValue.email_verified == true) {
-            const snapshot = await passwords.where('uid', '==', req.body.uid).get();
-            
-            let foundPassword = null;
-            snapshot.forEach((doc) => {
-                if (doc.id == req.body.id)
-                    foundPassword = doc.data();
-            })
-            if (foundPassword)
-                return res.status(200).json(foundPassword);
-            return res.status(400).json({ message: 'Unauthorized Access' });
+        if (decodeValue && decodeValue.email_verified == true) {
+            for (let i = 0; i < passkeys.length; i++) {
+                if (decodeValue.uid != passkeys[i].uid){
+                    valid = false;
+                    break;
+                }
+                passkeys[i].username = decrypt(passkeys[i].service, passkeys[i].username);
+                passkeys[i].password = decrypt(passkeys[i].service, passkeys[i].password);
+            }
         }
-    }
-    catch (e) {
-        return res.status(500).json({ message: 'Internal Server Error' });
+        if (valid == true)
+            res.status(200).json({ message: passkeys });
+        else
+            res.status(400).json({ message : 'Unauthorized access' })
+    } catch (error) {
+        res.status(500).json({ message: "Internal server error" });
     }
 });
 
-app.get('/test', (req,res) => {
+
+app.get('/test', (req, res) => {
     res.status(200).json({ message: 'Server is running.' });
 })
 
